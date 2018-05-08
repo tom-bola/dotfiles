7b9db794b6ffc7619588af8d2e708529f401a83b
Thomas Gebauer <tgebauer@apple.com>
2018-05-07 23:21:26 -0700

    Added prezto submodule and setup steps.

--- setup.sh
+++ setup.sh
@@ -10,3 +10,12 @@ ln -s ${DIR}/.config/nvim ~/.config
 
 grep -q '~/.bash_aliases' ~/.bashrc || \
   echo '[ -f ~/.bash_aliases ] && source ~/.bash_aliases' >> ~/.bashrc
+
+# Setup Prezto for zsh
+
+ln -s ${DIR}/prezto "${ZDOTDIR:-$HOME}/.zprezto"
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
