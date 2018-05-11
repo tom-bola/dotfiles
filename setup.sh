a3aac7c137b01e188dc7c9e7827cc3d97a88c9ae
Thomas Gebauer <tom.munich@googlemail.com>
2018-05-10 21:28:18 -0700

    setup: removed prezto stuff

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ ln -s ${DIR}/.vimrc ~
 ln -s ${DIR}/.tmux ~
 ln -s ${DIR}/.tmux.conf ~
 ln -s ${DIR}/.bash_aliases ~
+ln -s ${DIR}/.zshrc ~
 
 mkdir -p ~/.config
 ln -s ${DIR}/.config/nvim ~/.config
@@ -11,11 +12,3 @@ ln -s ${DIR}/.config/nvim ~/.config
 grep -q '~/.bash_aliases' ~/.bashrc || \
   echo '[ -f ~/.bash_aliases ] && source ~/.bash_aliases' >> ~/.bashrc
 
-# Setup Prezto for zsh
-
-ln -s ${DIR}/prezto "${ZDOTDIR:-$HOME}/.zprezto"
-
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
