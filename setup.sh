c17f9f39c08540add357779643f868d63abadeb1
Thomas Gebauer <tom.munich@googlemail.com>
2019-03-12 23:26:59 -0700

    setup: added homebrew + kegs

--- setup.sh
+++ setup.sh
@@ -14,3 +14,32 @@ ln -s ${DIR}/.config/nvim ~/.config
 grep -q '~/.bash_aliases' ~/.bashrc || \
   echo '[ -f ~/.bash_aliases ] && source ~/.bash_aliases' >> ~/.bashrc
 
+if [[ $(uname) = 'Darwin' ]]; then
+
+  # Install homebrew
+  if [[ -f '/usr/local/bin/brew' ]]; then
+    brew update
+  else
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
+
+  # Helper that either upgrades or installs a keg
+  vv=$(brew ls --versions)
+  function install_or_upgrade {
+    echo "Brewing $1..."
+    if [[ $vv =~ "$1 " ]]; then
+      HOMEBREW_NO_AUTO_UPDATE=1 brew upgrade "$1"
+    else
+      HOMEBREW_NO_AUTO_UPDATE=1 brew install "$1"
+    fi
+  }
+
+  install_or_upgrade neovim
+  install_or_upgrade reattach-to-user-namespace
+  install_or_upgrade autopep8
+  install_or_upgrade flake8
+  install_or_upgrade the_silver_searcher
+  install_or_upgrade tmux
+  install_or_upgrade tree
+  install_or_upgrade zsh-completions
+fi
