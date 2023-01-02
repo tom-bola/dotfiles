229d6ef052b7c33518ea58325415b69540758137
Thomas Gebauer <tom.munich@googlemail.com>
2023-01-02 23:57:34 +0100

    Added pylsp to install

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -337,7 +337,8 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-  -- clangd = {},
+  clangd = {},
+  pylsp = {},
   -- gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
